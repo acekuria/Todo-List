a2528e4e59ce831869707a311908585a36315bc4
AlvinKuria <alvinkuria18@gmail.com>
2023-07-12 22:41:10 +0100

    Html done and CSS nearly done

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hi, I\'m working')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLCtCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdIaSwgSVxcJ20gd29ya2luZycpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9

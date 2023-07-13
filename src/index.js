6860da780c0f5ae8d2305f4366886e705d80797c
AlvinKuria <alvinkuria18@gmail.com>
2023-07-13 19:19:18 +0100

    Project finished

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-  todos = JSON.parse(localStorage.getItem('todos')) || [];
+  todos = JSON.parse(localStorage.getItem('todos')) || []; 
   const nameInput = document.querySelector('#name');
   const newTodoForm = document.querySelector('#new-todo-form');
 
@@ -19,8 +19,13 @@ window.addEventListener('load', () => {
       done:false,
       createDate: new Date().getTime()
     }
-
-    todos.push(todo);
+    if (todo.content !== '') {
+      todos.push(todo);
+    }
+    else {
+      
+    }
+    
 
     localStorage.setItem('todos', JSON.stringify(todos))
 
@@ -37,7 +42,9 @@ function DisplayTodos() {
 
   todoList.innerHTML = '';
   
-  todos.forEach(todo=> {
+  todos.sort(function(a,b) {
+    return b.createDate - a.createDate;
+  }).forEach(todo=> {
     const todoItem = document.createElement('div');
     todoItem.classList.add('todo-item');
 
@@ -64,7 +71,7 @@ function DisplayTodos() {
     actions.classList.add('actions');
     edit.classList.add('edit');
     deleteButton.classList.add('delete');
-    content.innerHTML = `<input type="text" value=${todo.content}/>`;
+    content.innerHTML = `<input type="text" value='${todo.content}'readonly>`;
     edit.innerHTML = 'Edit';
     deleteButton.innerHTML = 'Delete';
 
@@ -93,7 +100,21 @@ function DisplayTodos() {
     })
     
     edit.addEventListener('click', e => {
-      const input = content.querySelector('input')
+      const input = content.querySelector('input');
+      input.removeAttribute('readonly');
+      input.focus();
+      input.addEventListener('blur', e => {
+        input.setAttribute('readonly', true);
+        todo.content = e.target.value;
+        localStorage.setItem('todos', JSON.stringify(todos));
+        DisplayTodos();
+      } )
+    })
+
+    deleteButton.addEventListener('click', e => {
+      todos = todos.filter(t => t != todo );
+      localStorage.setItem('todos', JSON.stringify.todos);
+      DisplayTodos();
     })
   })
 
